6c5ae70f89e65e5af3ebb2b6210651761195e4d5
Fredrik Ryman <fredrikw.ryman@gmail.com>
2026-06-21 22:34:47 +0200

    a11y: marquee-text klarar kontrast (large text 3:1)

--- src/components/home/MarqueeSection.jsx
+++ src/components/home/MarqueeSection.jsx
@@ -8,7 +8,7 @@ export default function MarqueeSection() {
       <div className="marquee-track">
         {[...words, ...words, ...words, ...words].map((word, i) => (
           <div key={i} className="flex items-center gap-8 px-8">
-            <span className="font-black font-display text-white/[0.06] whitespace-nowrap leading-none">
+            <span className="font-black font-display text-white/45 whitespace-nowrap leading-none">
               {word}
             </span>
             <span className="w-3 h-3 rounded-full bg-[#0078D4] animate-pulse-glow flex-shrink-0" />
